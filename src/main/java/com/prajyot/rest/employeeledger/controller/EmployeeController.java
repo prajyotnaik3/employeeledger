6a094afd78dbd3546a3c30daae149ecd55089f88
prajyotnaik3 <56463673+prajyotnaik3@users.noreply.github.com>
2020-11-03 03:24:45 +0530

    Update EmployeeController.java

--- src/main/java/com/prajyot/rest/employeeledger/controller/EmployeeController.java
+++ src/main/java/com/prajyot/rest/employeeledger/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.prajyot.rest.employeeledger.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.prajyot.rest.employeeledger.repository.EmployeeRepository;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
 public class EmployeeController {
 	
 	@Autowired
@@ -28,12 +30,14 @@ public class EmployeeController {
 	//Create an Employee
 	@PostMapping(value = "/addEmployee")
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public @ResponseBody String addEmployee(@RequestParam Integer employeeCode, @RequestParam String name, @RequestParam String designation) {
-		Employee n = new Employee(employeeCode, name, designation);
-	    employeeRepository.save(n);
+	public @ResponseBody String addEmployee(@RequestParam Employee employee) {
+	    employeeRepository.save(employee);
 	    return "Saved";
 	 }
+	//this command won't work anymore
 	//curl -X POST -d employeeCode=104 -d name="John White" -d designation="Web Developer" http://localhost:8080/addEmployee/
+	//Use following command
+	//curl -s -H "Content-Type: application/json;accept: application/json" -d "{\"employeeCode\":104,\"name\":\"John White\",\"designation\":\"Web Developer\"}" http://localhost:8080/addEmployee/
 	
 	//Read all the employees
 	@GetMapping(value = "/getEmployees")
